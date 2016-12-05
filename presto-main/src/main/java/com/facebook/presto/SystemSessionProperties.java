ff4ab337ff44fde640dc8d0445407ddcb161d19f
Piotr Nowojski <piotr.nowojski@teradata.com>
2016-12-05 10:18:58 +0100

    Add distribution_concurrency property

--- presto-main/src/main/java/com/facebook/presto/SystemSessionProperties.java
+++ presto-main/src/main/java/com/facebook/presto/SystemSessionProperties.java
@@ -45,6 +45,7 @@ public final class SystemSessionProperties
     public static final String PREFER_STREAMING_OPERATORS = "prefer_streaming_operators";
     public static final String TASK_WRITER_COUNT = "task_writer_count";
     public static final String TASK_CONCURRENCY = "task_concurrency";
+    public static final String DISTRIBUTION_CONCURRENCY = "distribution_concurrency";
     public static final String TASK_SHARE_INDEX_LOADING = "task_share_index_loading";
     public static final String QUERY_MAX_MEMORY = "query_max_memory";
     public static final String QUERY_MAX_RUN_TIME = "query_max_run_time";
@@ -157,6 +158,11 @@ public final class SystemSessionProperties
                             return concurrency;
                         },
                         value -> value),
+                integerSessionProperty(
+                        DISTRIBUTION_CONCURRENCY,
+                        "Default number of local parallel partitioners per local exchange",
+                        1,
+                        false),
                 booleanSessionProperty(
                         TASK_SHARE_INDEX_LOADING,
                         "Share index join lookups and caching within a task",
@@ -322,6 +328,11 @@ public final class SystemSessionProperties
     public static int getTaskConcurrency(Session session)
     {
         return session.getSystemProperty(TASK_CONCURRENCY, Integer.class);
+    }
+
+    public static int getDistributionConcurrency(Session session)
+    {
+        return session.getSystemProperty(DISTRIBUTION_CONCURRENCY, Integer.class);
     }
 
     public static boolean isShareIndexLoading(Session session)
