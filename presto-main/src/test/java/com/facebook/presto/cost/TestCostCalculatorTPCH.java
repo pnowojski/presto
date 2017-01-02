1e1782965efe45a11ce81638295b3a71e45df1df
Piotr Nowojski <piotr.nowojski@teradata.com>
2017-01-02 14:25:06 +0100

    mid work tpch cost calculator

--- presto-main/src/test/java/com/facebook/presto/cost/TestCostCalculatorTPCH.java
+++ presto-main/src/test/java/com/facebook/presto/cost/TestCostCalculatorTPCH.java
@@ -13,6 +13,72 @@
  */
 package com.facebook.presto.cost;
 
+import com.facebook.presto.spi.statistics.Estimate;
+import com.facebook.presto.sql.planner.LogicalPlanner;
+import com.facebook.presto.sql.planner.Plan;
+import com.facebook.presto.sql.planner.assertions.PlanAssert;
+import com.facebook.presto.sql.planner.assertions.PlanMatchPattern;
+import com.facebook.presto.sql.planner.plan.FilterNode;
+import com.facebook.presto.testing.LocalQueryRunner;
+import com.facebook.presto.tpch.TpchConnectorFactory;
+import com.google.common.collect.ImmutableMap;
+import org.testng.annotations.Test;
+
+import static com.facebook.presto.spi.statistics.Estimate.unknownValue;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.anyTree;
+import static com.facebook.presto.sql.planner.assertions.PlanMatchPattern.node;
+import static com.facebook.presto.testing.TestingSession.testSessionBuilder;
+
 public class TestCostCalculatorTPCH
 {
+    private final LocalQueryRunner queryRunner;
+    private final CostCalculator costCalculator;
+
+    public TestCostCalculatorTPCH()
+    {
+        this.queryRunner = new LocalQueryRunner(testSessionBuilder()
+                .setCatalog("local")
+                .setSchema("tiny")
+                .build());
+
+        queryRunner.createCatalog(queryRunner.getDefaultSession().getCatalog().get(),
+                new TpchConnectorFactory(1, true),
+                ImmutableMap.<String, String>of());
+
+        costCalculator = new CoefficientBasedCostCalculator(queryRunner.getMetadata());
+    }
+
+    @Test
+    public void testCostCalculatorUsesLayout()
+    {
+        assertPlan("SELECT orderstatus FROM orders WHERE orderstatus = 'P'",
+                anyTree(
+                        node(FilterNode.class, anyTree())
+                                .withCost(PlanNodeCost.builder()
+                                        .setOutputRowCount(new Estimate(385.0))
+                                        .setOutputSizeInBytes(unknownValue())
+                                        .build())));
+
+        assertPlan("SELECT orderstatus FROM orders WHERE orderkey = 42",
+                anyTree(
+                        node(FilterNode.class, anyTree())
+                                .withCost(PlanNodeCost.builder()
+                                        .setOutputRowCount(new Estimate(0.0))
+                                        .setOutputSizeInBytes(unknownValue())
+                                        .build())));
+    }
+
+    private void assertPlan(String sql, PlanMatchPattern pattern)
+    {
+        assertPlan(sql, LogicalPlanner.Stage.CREATED, pattern);
+    }
+
+    private void assertPlan(String sql, LogicalPlanner.Stage stage, PlanMatchPattern pattern)
+    {
+        queryRunner.inTransaction(transactionSession -> {
+            Plan actualPlan = queryRunner.createPlan(transactionSession, sql, stage);
+            PlanAssert.assertPlan(transactionSession, queryRunner.getMetadata(), costCalculator, actualPlan, pattern);
+            return null;
+        });
+    }
 }
